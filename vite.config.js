692431e6cdc51c5c33a163f3088ed9d7f856ff69
ansari taufique <ansaritaufique828@gmail.com>
2024-12-23 01:06:15 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'  // This will make Vite output the build to the 'build' folder
+  }
 })
